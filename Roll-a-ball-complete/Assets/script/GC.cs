c0b690afbfd2d8056d1b7066220ca1f20d2821bf
apresent-PC\apresent <amailforxx@gmail.com>
2017-09-23 18:37:52 +0900

    no message

---
+++ Roll-a-ball-complete/Assets/script/GC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GC : MonoBehaviour {
+	public UnityEngine.UI.Text scoreLabel;
+	public GameObject winnerLabelObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+		scoreLabel.text = count.ToString ();
+
+		if (count == 0) {
+			// クリア時の処理
+			winnerLabelObject.SetActive (true);
+		}
+	}
+}
